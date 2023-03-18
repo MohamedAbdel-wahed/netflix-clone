a094e952e05fb98c56a03dc3f5f64c81a7e793e9
MohamedAbdel-wahed <webwebdeveloper.int99@gmail.com>
2023-03-18 11:01:56 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {requests} from './utils/endpoints'
 
 function App() {
 
-  const [loading,setLoading]= useState(false);
+  const [loading,setLoading]= useState(true);
 
   useEffect(()=>{
     window.onload= ()=> setLoading(false)
